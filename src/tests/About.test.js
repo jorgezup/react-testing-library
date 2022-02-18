d45eaf11f07dbb14ad8c5c1e88eb22719deeecde
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-02-18 15:45:34 -0400

    feat: created test to About Component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste do Component About', () => {
+  test('Verifica se a página contém um h2 com o texto "About Pokédex"', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const aboutTitle = getByRole('heading', { name: 'About Pokédex', level: 2 });
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('Verifica se a página contém dois parágrafos com texto sobre a pokédex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+
+    const paragraphs = getAllByText(/pokémons/i);
+
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  test('Verifica se a imagem recebe o determinado "src" ', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const image = getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
